07814e30dee8afa17bf2181310c1bd06779ed190
hrachmnam <157538135+hrachmnam@users.noreply.github.com>
2024-02-16 12:07:13 +0100

    Add files via upload

---
+++ obdelnik.py
@@ -0,0 +1,8 @@
+a = int(input("zadej délku první strany"))
+b = int(input("zadej délku druhé strany"))
+obvod = 2 * (a + b)
+obsah = a*b
+print("obvod je:")
+print(obvod)
+print("obsah je:")
+print(obsah)
