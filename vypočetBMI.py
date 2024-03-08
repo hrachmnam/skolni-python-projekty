ceeb6dfce1c7cea2eb5c9b81610b18b28494d555
hrachmnam <157538135+hrachmnam@users.noreply.github.com>
2024-03-08 12:11:19 +0100

    Create vypočetBMI.py

---
+++ vypočetBMI.py
@@ -0,0 +1,26 @@
+print("Dobrý den")
+print("Tento program určí vaše BMI(body mass index)")
+print("")
+print("zadejte vaši výšku v cm")
+height_in_cm = int(input())
+print("zadejte vaši hmotnost v kg")
+weight = int(input())
+height_in_m = (height_in_cm / 100)
+bmi = round((weight / (height_in_m * height_in_m)) , 2)
+print("vaše bmi je: " + str(bmi))
+if bmi <= 16.5:
+    print("máte těžkou podvýživu")
+elif bmi > 16.5 and bmi < 18.5:
+    print("máte podváhu")
+elif bmi > 18.5 and bmi < 25:
+    print("máte ideální (zdravou) váhu")
+elif bmi > 25 and bmi < 30:
+    print("máte nadváhu")
+elif bmi > 30 and bmi < 35:
+    print("máte obezitu prvního stupně")
+elif bmi > 35 and bmi < 40:
+    print("máte obezitu druhého stupně")
+elif bmi > 40:
+    print("máte obezitu třetího stupně (též morbidní obezitu)")
+else:
+    print("ERROR")
