4c02707b48a5af01b7c5da2151450e0a09e57544
hrachmnam <157538135+hrachmnam@users.noreply.github.com>
2024-03-22 11:19:53 +0100

    Create dělení_dvou_čísel.py

---
+++ dělení_dvou_čísel.py
@@ -0,0 +1,9 @@
+print("zadejte dělenec")
+a = int(input())
+print("zadejte dělitel")
+b = int(input())
+if (a == 0 or b == 0):
+    print(":)")
+else:
+    c = round(a/b,5)
+    print("podíl je: " + str(c))
