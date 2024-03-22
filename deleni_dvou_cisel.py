9f6ca53d8c74b18f899a6716698e41b861b71f3e
hrachmnam <157538135+hrachmnam@users.noreply.github.com>
2024-03-22 11:59:51 +0100

    Update deleni_dvou_cisel.py

--- deleni_dvou_cisel.py
+++ deleni_dvou_cisel.py
@@ -3,7 +3,7 @@ a = int(input())
 print("zadejte dělitel")
 b = int(input())
 if (a == 0 or b == 0):
-    print(":)")
+    print("ERROR")
 else:
     c = round(a/b,5)
     print("podíl je: " + str(c))
